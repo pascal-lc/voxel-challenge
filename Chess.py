20a9cac0ba433b9130fe8b980a8874fe29d56783
pascal-lc <pascal.lc@outlook.com>
2022-05-14 11:46:39 +0800

    代码优化，光线、颜色调整

--- Chess.py
+++ Chess.py
@@ -2,7 +2,7 @@ from scene import Scene; import taichi as ti; from taichi.math import *
 scene = Scene(voxel_edges=0.0, exposure=3)
 scene.set_background_color(color=(0.75, 0.75, 0.75))
 scene.set_floor(height=-0.05, color=(0.3, 0.3, 0.3))
-scene.set_directional_light(direction=(1, 1, 0), direction_noise=0.2, color=(0.8, 0.7, 0.6))
+scene.set_directional_light(direction=(1, 1, 0), direction_noise=0.2, color=(0.5, 0.5, 0.5))
 @ti.func
 def set_blocks(st, ed, mat, color, color_noise=vec3(0.1)):
   for v in ti.grouped(ti.ndrange((st[0], ed[0]), (st[1], ed[1]), (st[2], ed[2]))): scene.set_voxel(v, mat, color)
@@ -62,36 +62,33 @@ def piece(origin, radius, color, color_noise=vec3(0.1)):
   for v in ti.grouped(ti.ndrange((origin[0] - radius[0], origin[0] + radius[0]),
       (origin[1] - 2*radius[1], origin[1] + 2*radius[1]), (origin[2] - radius[2], origin[2] + radius[2]))):
     if is_conicoid(origin, v, radius[0], vec3(1, 4, 1)): scene.set_voxel(v, 1, color + color_noise*ti.random())
+
 @ti.kernel
 def initialize_voxels():
   N = 8; H = 1; scale = 16
   black, white = vec3(0.0, 0.0, 0.0), vec3(1.0, 1.0, 1.0)
   for i, j in ti.ndrange(N, N):
-    if (i + j) % 2: checkerboard(ivec3(i, -2, j), scale, white)
-    else: checkerboard(ivec3(i, -2, j), scale, black)
-  radius_pawn = ivec3(6, 12, 6); color_me = vec3(0.82, 0.55, 0.27); color_noise = vec3(0.5)
-  for i in range(8):
-    pawn(ivec3((float(i)+0.5)*scale, 1, 64-3*scale/2), radius_pawn, color_me, color_noise)
-    piece(ivec3((float(i)+0.5)*scale, 1, 64-scale/2), radius_pawn, color_me, color_noise)
-  knight(ivec3(-64+(1+0.5)*scale, 17, 64-scale/2), ivec3(4), color_me, color_noise)
-  knight(ivec3(64-(1+0.5)*scale, 17, 64-scale/2), ivec3(4), color_me, color_noise)
-  bishop(ivec3(-64+(2+0.5)*scale, 17, 64-scale/2), ivec3(4), color_me, color_noise)
-  bishop(ivec3(64-(2+0.5)*scale, 17, 64-scale/2), ivec3(4), color_me, color_noise)
-  rook(ivec3(-64+(0.5)*scale, 17, 64-scale/2), ivec3(4), color_me, color_noise)
-  rook(ivec3(64-(0.5)*scale, 17, 64-scale/2), ivec3(4), color_me, color_noise)
-  queen(ivec3(-64+(3+0.5)*scale, 17, 64-scale/2), ivec3(4), color_me, color_noise)
-  king(ivec3(-64+(4+0.5)*scale, 17, 64-scale/2), ivec3(4), color_me, color_noise)
-  color_enemy = vec3(0.1)
-  for i in range(8):
-    pawn(ivec3((float(i)+0.5)*scale, 1, -(64-3*scale/2)), radius_pawn, color_enemy, color_noise)
-    piece(ivec3((float(i)+0.5)*scale, 1, -(64-scale/2)), radius_pawn, color_enemy, color_noise)
-  knight(ivec3(-64+(1+0.5)*scale, 17, -(64-scale/2)), ivec3(4), color_enemy, color_noise)
-  knight(ivec3(64-(1+0.5)*scale, 17, -(64-scale/2)), ivec3(4), color_enemy, color_noise)
-  bishop(ivec3(-64+(2+0.5)*scale, 17, -(64-scale/2)), ivec3(4), color_enemy, color_noise)
-  bishop(ivec3(64-(2+0.5)*scale, 17, -(64-scale/2)), ivec3(4), color_enemy, color_noise)
-  rook(ivec3(-64+(0.5)*scale, 17, -(64-scale/2)), ivec3(4), color_enemy, color_noise)
-  rook(ivec3(64-(0.5)*scale, 17, -(64-scale/2)), ivec3(4), color_enemy, color_noise)
-  queen(ivec3(-64+(3+0.5)*scale, 17, -(64-scale/2)), ivec3(4), color_enemy, color_noise)
-  king(ivec3(-64+(4+0.5)*scale, 17, -(64-scale/2)), ivec3(4), color_enemy, color_noise)
+    if (i + j) % 2: checkerboard(ivec3(i, -2, j), scale, black)
+    else: checkerboard(ivec3(i, -2, j), scale, white)
+  radius_pawn = ivec3(6, 12, 6); color_noise = vec3(0.1)
+  color_me = vec3(ti.hex_to_rgb(0xAF5E24)); color_enemy = vec3(0.1)
+
+  for i in range(16):
+    sign, color = -1, color_enemy
+    if i < 8: sign, color = 1, color_me
+    pawn(ivec3(((i%8) + 0.5)*scale, 1, sign*(64-3*scale/2)), radius_pawn, color, color_noise)
+    piece(ivec3(((i%8) + 0.5)*scale, 1, sign*(64-scale/2)), radius_pawn, color, color_noise)
+
+  for i in range(2):
+    color = color_enemy if i == 0 else color_me
+    knight(ivec3(-64+(1+0.5)*scale, 17, (2*i-1)*(64-scale/2)), ivec3(4), color, color_noise)
+    knight(ivec3(64-(1+0.5)*scale, 17, (2*i-1)*(64-scale/2)),ivec3(4), color, color_noise)
+    bishop(ivec3(-64+(2+0.5)*scale, 17, (2*i-1)*(64-scale/2)), ivec3(4), color, color_noise)
+    bishop(ivec3(64-(2+0.5)*scale, 17, (2*i-1)*(64-scale/2)), ivec3(4), color, color_noise)
+    rook(ivec3(-64+(0.5)*scale, 17, (2*i-1)*(64-scale/2)), ivec3(4), color, color_noise)
+    rook(ivec3(64-(0.5)*scale, 17, (2*i-1)*(64-scale/2)), ivec3(4), color, color_noise)
+    queen(ivec3(-64+(3+0.5)*scale, 17, (2*i-1)*(64-scale/2)), ivec3(4), color, color_noise)
+    king(ivec3(-64+(4+0.5)*scale, 17, (2*i-1)*(64-scale/2)), ivec3(4), color, color_noise)
+
 initialize_voxels()
 scene.finish()
